0c56d43b6bbeaab223124077efd28c311addf3d5
tom avilius <tomavilius@tomavilius.in>
2025-06-17 10:03:54 +0530

    Scaffold App Bar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <HelloWorld />
-</template>
+  <v-app-bar>
+    <v-row justify="center" align="center">
+      <v-btn to="/" text>Home</v-btn>
+      <v-btn to="/about" text>About</v-btn>
+      <v-btn to="/report" text>Report</v-btn>
+    </v-row>
+  </v-app-bar>
 
-<script lang="ts" setup>
-  //
-</script>
+  <h1 class="text-h1">Main Title</h1>
+</template>

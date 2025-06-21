f56bd4d4cc573d966b748fa0ddd0f478fa1dad20
tom avilius <tomavilius@tomavilius.in>
2025-06-21 22:46:23 +0530

    Add report button

--- src/components/result/Header.vue
+++ src/components/result/Header.vue
@@ -9,10 +9,17 @@
     ></v-text-field>
     <h4 class="text-h4 subtitle">The claim is probably {{ verdict }}.</h4>
     <h6 class="caption">We uncovered {{ report }} evidence.</h6>
+    <v-btn class="btn" append-icon="mdi-forum" variant="plain" to="/report"
+      >Not what you were expecting?</v-btn
+    >
   </v-container>
 </template>
 
 <style scoped>
+.btn {
+  letter-spacing: 1px;
+  font-size: 0.8rem !important;
+}
 .subtitle {
   font-size: 2.8rem !important;
   font-weight: 300;

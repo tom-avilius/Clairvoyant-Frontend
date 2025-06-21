cbdf7d33fa61f7833a0e7650839a4c7c47e902ff
tom avilius <tomavilius@tomavilius.in>
2025-06-21 10:28:04 +0530

    Document Form.vue

--- src/components/report/Form.vue
+++ src/components/report/Form.vue
@@ -2,7 +2,6 @@
   <v-form @submit.prevent>
     <v-text-field
       class="spacing"
-      :rules="[rule]"
       v-model="query"
       label="Your query"
       variant="outlined"
@@ -37,6 +36,9 @@
 <script setup>
 import { ref, watch } from "vue";
 
+// a list of errors that may occur
+// for the user to select and report.
+// WARN: Not a complete or even tested list.
 const errors = [
   "Fake claim labelled as true.",
   "True claim labelled as false.",
@@ -44,60 +46,62 @@ const errors = [
   "Something Else",
 ];
 
+// the loading state of the submit btn
 const loading = ref(false);
+// the constraints for the selection input element
 const selectedError = ref(null);
+// flag to display the text area
 const showTextArea = ref(false);
+// the query entered by the user in the
+// home page
 const query = ref("");
+// to display the contraint errors using the
+// snackbar.
 const errorMsg = ref([]);
+// contraints for the text area input
 const errorDescription = ref("");
 
-const rule = (v) => {
-  const wordCount = v.trim().split(/\s+/).filter(Boolean).length;
 
-  if (wordCount === 0) return "Query cannot be empty.";
-  if (wordCount < 4) return "Query must contain at least 4 words.";
-  if (wordCount > 100) return "Query must be below 100 words.";
-  return true;
-};
-
+// to evaluate constraints for the selection element
 const selectedErrorRule = (v) => {
   if (v === null) return "Error type cannot be empty.";
   return true;
 };
 
+// to evaluate contraints for the text area element
 const errorDescriptionRule = (v) => {
   if (v.trim().length < 20) return "Please enter atleast 20 characters.";
   return true;
 };
 
+// handle submit btn click
 async function load() {
+  // set the loading state
   loading.value = true;
 
-  const wordCount = query.value.trim().split(/\s+/).filter(Boolean).length;
-
-  if (wordCount == 0) {
-    errorMsg.value.push("Query cannot be empty.");
-  } else if (wordCount < 4) {
-    errorMsg.value.push("Query must contain atleast 4 words.");
-  } else if (wordCount > 100) {
-    errorMsg.value.push("Query must be below 100 words.");
-  }
-
+  // display selection input error if any
   if (selectedError.value == null) {
     errorMsg.value.push("Error type cannot be empty.");
   }
 
+  // display text area errors if any
   if (errorDescription.value.trim().length < 20) {
     errorMsg.value.push(
       "Error description must contain atleast 20 characters.",
     );
   }
 
+  // ensure the loading state is atleast 0.2s
   await new Promise((resolve) => setTimeout(resolve, 200));
+  // disable the loading state
   loading.value = false;
 }
 
-watch(selectedError, (newError, oldError) => {
+
+// to control when to display the text area
+watch(selectedError, (newError) => {
+
+  // only display text area for bottom two errors
   if (newError == errors[2] || newError == errors[3]) {
     showTextArea.value = true;
   } else {
